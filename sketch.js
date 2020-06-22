86ea02c31da9f2e3b7b3a8387f5f9bb914a898bd
Sam <sweatersjpg@gmail.com>
2020-06-22 01:24:37 -0400

    added loading of files, it took a while...

--- sketch.js
+++ sketch.js
@@ -1,16 +1,57 @@
 // Cat Creator
 // qbttr
 
-function preload() {
-  // Hello girlfriend
-  let prefix = "https://qbttr.github.com/catcreator";
-  let suffix = ".png";
-  let folders = [
-    "Body", "Ears", "Hair", "Head", "Face/Eyes", "Face/Mouth", "Face/Nose", "Tail",
-    "Accessories/Body", "Accessories/Hair", "Accessories/Head",
-    "Legs/FrontLeft", "Legs/FrontRight", "Legs/BackLeft", "Legs/BackRight"
-  ]
 
+let folders = {
+  "Accessories/Body/back": { count:4, content:"line base" },
+  "Accessories/Body/collar": { count:5, content:"line base colour" },
+  "Accessories/Hair":{ count:5, content:"line base colour" },
+  "Accessories/Head":{ count:1, content:"base"},
+  "Body": { count:1, content:"line base", patterns:13 , overlays:10 },
+  "Ears": { count:5, content:"line base", patterns:3 , overlays:4 },
+  "Face/Eyes": { count:4, content:"line base" },
+  "Face/Mouth": { count:4, content:"line" },
+  "Face/Nose": { count:3, content:"line" },
+  "Hair": { count:2, content:"line base" },
+  "Head":{ count:1, content:"line base", patterns:5 , overlays:7, accents: 3},
+  "Legs/LeftBack": { count:1, content:"line base", patterns:3 , overlays:4 },
+  "Legs/LeftFront": { count:1, content:"line base", patterns:3 , overlays:4 },
+  "Legs/RightBack": { count:1, content:"line base", patterns:3 , overlays:4 },
+  "Legs/RightFront": { count:1, content:"line base", patterns:3 , overlays:4 },
+  "Tail":{ count:6, content:"line base", patterns:3, overlays:4 },
+};
+
+let images = {};
+
+function preload() {
+
+  for (var k of Object.keys(folders)) {
+    let folder = folders[k];
+    n = toLowerCase(k);
+    images[n] = new Array(folder.count);
+    for (var i = 0; i < images[n].length; i++) {
+      images[n][i] = getImages(i, k, folder);
+    }
+  }
+
+  function getImages(i, folder, info) {
+    let prefix = "https://qbttr.github.com/catcreator/Assets";
+    let suffix = ".png";
+
+    let item = {};
+
+    for (var c of ["line", "base", "colour"])
+    if(info.content.includes(c)) item[c] = loadImage(prefix+folder+"/"+i+"/" +c+suffix);
+
+    for (var c of ["patterns", "overlays", "accents"])
+    if(info.hasOwnProperty(c)) {
+      item[c] = new Array(info[c]);
+      for (var i = 0; i < items[c].length; i++)
+      item[c][i] = loadImage(prefix+folder+"/"+i+"/"+c+"/"+i+suffix);
+    }
+
+    return item;
+  }
 
 }
 
